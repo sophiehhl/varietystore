27da4d5153027e16c263472c6c348ad5105315de
Sophie Loan Ho <70752927+sophiehhl@users.noreply.github.com>
2020-12-04 14:37:35 -0800

    Create LineItem.java

---
+++ src/java/variety/business/LineItem.java
@@ -0,0 +1,60 @@
+
+package variety.business;
+
+import java.text.NumberFormat;
+import java.io.Serializable;
+
+public class LineItem implements Serializable {
+
+    private Long lineItemId;
+    private Product product;
+    private Product name;
+    private int quantity = 1;
+
+    public LineItem() {
+    }
+
+    public Long getLineItemId() {
+        return lineItemId;
+    }
+
+    public void setLineItemId(Long lineItemId) {
+        this.lineItemId = lineItemId;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setName(Product product) {
+        this.name = name;
+    }
+
+    public Product getName() {
+        return name;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getTotal() {
+        double total = product.getPrice() * quantity;
+        return total;
+    }
+
+    public String getTotalCurrencyFormat() {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        return currency.format(this.getTotal());
+    }
+
+
+}
